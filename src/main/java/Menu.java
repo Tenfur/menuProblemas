495dda560718a9f1de26afbdc68afd12c44b5d35
Paolo <Paolo>
2020-02-16 00:53:58 -0500

    Segunda avance

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -10,6 +10,14 @@ public class Menu {
             case 2: menu_2(); break;
             case 3: menu_3(); break;
             case 4: menu_4(); break;
+            case 5: menu_5(); break;
+            case 6: menu_6(); break;
+            case 7: menu_7(); break;
+            case 8: menu_8(); break;
+            case 9: menu_9(); break;
+            case 10: menu_10(); break;
+            
+            
         }
         
     }
@@ -18,6 +26,12 @@ public class Menu {
         System.out.println("2) Calcular que numero es mayor o menor");
         System.out.println("3) Calculadora");
         System.out.println("4) Comprobar si una letra es mayúscula o no");
+        System.out.println("5) Adivinar un numero random");
+        System.out.println("6) Imprimir un texto n veces");
+        System.out.println("7) Calcular si un numero es primo o no");
+        System.out.println("8) Calcular N numeros primos");
+        System.out.println("9) Pedir una tabla de multiplicar");
+        System.out.println("10) Calcular N tablas de multiplicar");
         
         
         System.out.print("Ingrese su opcion: ");
@@ -101,5 +115,85 @@ public class Menu {
         else
             System.out.println("La letra no es masyúscula!");
     }
-   
+    public static void menu_5(){
+        Scanner sc = new Scanner(System.in);
+        int random = (int)(Math.random() * 100), numero, cont = 0;
+        do{
+            System.out.print("Ingrese un numero: ");
+            numero =sc.nextInt();
+            if(numero > random){
+                System.out.println("El numero es menor");
+            }
+            else
+                System.out.println("El numero es mayor");
+            
+            cont++;
+        }while(numero != random);
+        System.out.println("Ganaste en " + cont + " intentos");
+    }
+   public static void menu_6(){
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Ingrese la cantidad de veces que se va a imprimir el texto: ");
+       int num = sc.nextInt();
+       System.out.println("Ingrese el texto: ");
+       String text = "Hola mundo";
+       for(int i = 1; i <= num; i++){
+           System.out.println(text);
+       }
+   }
+   public static void menu_7(){
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Ingrese un numero y te diremos si es primo o no: ");
+       int num = sc.nextInt();
+       int cont = 0;
+       for(int i = 1; i <= num; i++){
+           if(num % i == 0){
+               cont++;
+           }
+       }
+       if(cont == 2){
+           System.out.println("El numero es primo"); 
+       }
+       else{
+           System.out.println("El numero no es primo");
+           System.out.println("Tiene " + cont + " divisores");
+       }
+   }
+   public static void menu_8(){
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Te diremos la cantidad de numeros primos que hay hasta un numero: ");
+       int num =sc.nextInt();
+       int cont = 0;
+        System.out.println("Numeros primos: ");
+       for(int i = 1; i <= num ; i++){
+           for(int j = i; j >= 1; j--){
+              if(i % j == 0){
+                  cont++;
+              }
+           }
+           if(cont == 2)
+               System.out.println(i);
+           
+           cont = 0;
+       }   
+   }
+   public static void menu_9(){
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Pide un numero y te daremos su tabla de multiplicar: ");
+       int num = sc.nextInt();
+       for(int i = 0; i <= 12; i++){
+           System.out.println(num + " x " + i + " = " + (i * num));
+       }
+   }
+   public static void menu_10(){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Ingrese el numero de tablas que quiere: ");
+       int num = sc.nextInt();
+       for(int i = 0; i <= num; i++){
+           System.out.println("Tabla numero " + i + ": ");
+           for(int j = 1; j <=12; j++){
+               System.out.println(i + " x " + j + " = " + (j * i));
+           }
+       }
+   }
 }
