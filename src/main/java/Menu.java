f4ed7b75cdc6cc92f63a9f92c62c17694385f6ef
Paolo <Paolo>
2020-02-16 23:58:05 -0500

    Tercer avance

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -16,6 +16,8 @@ public class Menu {
             case 8: menu_8(); break;
             case 9: menu_9(); break;
             case 10: menu_10(); break;
+            case 11: menu_11(); break;
+            case 12: menu_12(); break;
             
             
         }
@@ -32,6 +34,8 @@ public class Menu {
         System.out.println("8) Calcular N numeros primos");
         System.out.println("9) Pedir una tabla de multiplicar");
         System.out.println("10) Calcular N tablas de multiplicar");
+        System.out.println("11) Cajero");
+        System.out.println("12) Figuras");
         
         
         System.out.print("Ingrese su opcion: ");
@@ -194,6 +198,96 @@ public class Menu {
            for(int j = 1; j <=12; j++){
                System.out.println(i + " x " + j + " = " + (j * i));
            }
+       }
+   }
+   public static void menu_11(){
+       Scanner sc = new Scanner(System.in);
+       int opcion, opcion_soles, opcion_dolares;
+       double dinero, cuenta_soles = 0, cuenta_dolares = 0;
+       do{
+       System.out.println("Bievenido al cajero!");
+       System.out.println("1) Cuenta en soles");
+       System.out.println("2) Cuenta en dolares");
+       System.out.println("3) Salir");
+       System.out.print("Ingrese la opcion: ");
+       opcion =sc.nextInt();
+       switch(opcion){
+           case 1: do{
+                   System.out.println("Cuenta en soles");
+                   System.out.println("1) Ingresar dinero");
+                   System.out.println("2) Retirar dinero");
+                   System.out.println("3) Ver saldo");
+                   System.out.println("4) Salir");
+                   System.out.print("Ingrese la opcion: ");
+                   opcion_soles = sc.nextInt();
+                   if(opcion_soles == 1){
+                       System.out.println("Ingrese su dinero: ");
+                       dinero = sc.nextDouble();
+                       cuenta_soles +=dinero;
+                   }
+                   else if(opcion_soles == 2){
+                       System.out.println("Ingrese la cantidad que quiere retirar");
+                       dinero = sc.nextDouble();
+                       if(dinero > cuenta_soles){
+                           System.out.println("No tiene ese dinero");
+                       }
+                       else
+                           cuenta_soles -=dinero;
+                     }
+                   else if(opcion_soles == 3){
+                       System.out.println("Tiene: " + cuenta_soles + " soles");
+                   }
+                   }while(opcion_soles != 4);
+                   break;
+                   
+           case 2: do{
+                   System.out.println("Cuenta en dolares");
+                   System.out.println("1) Ingresar dinero");
+                   System.out.println("2) Retirar dinero");
+                   System.out.println("3) Ver saldo");
+                   System.out.println("4) Salir");
+                   System.out.print("Ingrese la opcion: ");
+                   opcion_dolares = sc.nextInt();
+                   if(opcion_dolares == 1){
+                       System.out.print("Ingrese su dinero: ");
+                       dinero = sc.nextDouble();
+                       cuenta_dolares +=dinero;
+                   }
+                   else if(opcion_dolares == 2){
+                       System.out.print("Ingrese la cantidad que quiere retirar: ");
+                       dinero = sc.nextDouble();
+                       if(dinero > cuenta_dolares){
+                           System.out.println("No tiene esa cantidad");
+                       }
+                       else
+                           cuenta_dolares -=dinero;
+                   }
+                   else if(opcion_dolares == 3)
+                       System.out.println("Tiene: " + cuenta_dolares + " dolares");
+                   
+                  }while(opcion_dolares != 4);
+       }
+       
+       }while(opcion != 3);
+       System.out.println("Gracias por usar el cajero!");
+       System.out.println("Su saldo final en soles es: " + cuenta_soles);
+       System.out.println("Su saldo final en dolares es: " + cuenta_dolares);
+   }
+   public static void menu_12(){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Menu de figuras");
+       System.out.println("1) Cuadrado");
+       System.out.print("Ingrese opcion: ");
+       int opcion = sc.nextInt();
+       switch(opcion){
+           case 1: for(int i = 0; i <= 5; i++){
+                    for(int j = 0; j <= 5; j++){
+                        if(i == 0 || j == 0 || i == 5 || j == i){
+                            System.out.print("*");
+                        }
+                    }
+                    System.out.println("");
+                  }
        }
    }
 }
